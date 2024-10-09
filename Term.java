1a98b43d90805e748844550735fbc9dd6231eb77
benseidenberg <ben.seidenberg@gmail.com>
2024-10-08 20:10:26 -0400

    testing 3

--- Term.java
+++ Term.java
@@ -6,6 +6,7 @@ public class Term implements Comparable<Term> {
     public Term(String query, long weight) {
         // hello, testing 1
         // Hello >)
+        // hello 3
     }
 
     // Compares the two terms in descending order by weight.
